ccc6afc70b5aa8f54475afa7f256f8ba37135986
PraveenKumar <pb.praveen@xymob.in>
2021-03-16 01:28:44 +0530

    Will update the IP for incoming

--- express.js
+++ express.js
@@ -13,7 +13,7 @@ const path = require('path');
 
 
 //const IVRRECEIVEURL3 = "http://138.68.245.156:5000"
-const IVRRECEIVEURL3 = 'http://157.245.219.158:5000';
+const IVRRECEIVEURL3 = 'http://165.232.145.164:5000';
 
 log4js.configure({
   appenders: {
